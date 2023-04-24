cc44ef720add4d3d3b3b9e312411bc4c68c77b31
ssg-js <likemath1997@gmail.com>
2023-04-24 16:58:35 +0900

    FEAT: 네비게이터 설정

---
+++ another_fe/lib/screens/record/record.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class RecordTab extends StatelessWidget {
+  const RecordTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('Records Tab'),
+      ),
+    );
+  }
+}
