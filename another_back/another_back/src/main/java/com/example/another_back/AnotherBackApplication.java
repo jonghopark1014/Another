73f6723b038a8fb213582a25c1d6f14c972ab450
왕현석 <sjj02055@gmail.com>
2023-04-24 17:56:31 +0900

    feature : 엔티티 설정 및 연관관계 설정

---
+++ another_back/another_back/src/main/java/com/example/another_back/AnotherBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.another_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnotherBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnotherBackApplication.class, args);
+	}
+
+}
---
+++ another_back/another_back/src/test/java/com/example/another_back/AnotherBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.another_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotherBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
