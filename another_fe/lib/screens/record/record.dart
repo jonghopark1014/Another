bb586c3bc606f8048c7b3adccb50e00559797c84
baamkyu <baamkyu@gmail.com>
2023-04-24 17:54:25 +0900

    FEAT: 회원가입 페이지 UI 구성

--- another_fe/lib/screens/record/record.dart
+++ another_fe/lib/screens/record/record.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import '../account/login.dart';
 
 class RecordTab extends StatelessWidget {
   const RecordTab({Key? key}) : super(key: key);
@@ -8,7 +8,22 @@ class RecordTab extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-        child: Text('Records Tab'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Records Tab'),
+            ElevatedButton(
+              onPressed: () {
+                // 로그인 페이지로 이동하는 로직을 작성
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginPage()),
+                );
+              },
+              child: Text('로그인 페이지'),
+            ),
+          ],
+        ),
       ),
     );
   }
