425072a3219ab8d993ba7e864866108c99fc9157
baamkyu <baamkyu@gmail.com>
2023-04-30 17:40:12 +0900

    FEAT: 달력 렌더링 & 프로필 경험치바 추가

---
+++ another_front/lib/screens/record/widgets/category_title.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:another/constant/color.dart';
+
+class CategoryTitle extends StatelessWidget {
+  final title;
+  CategoryTitle({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 20),
+          Container(
+            margin: EdgeInsets.only(bottom: 10),
+            child: Text(
+              '$title',
+              style: TextStyle(
+                color: WHITE_COLOR,
+                fontSize: 16,
+              ),
+            ),
+          ),
+        ]
+    );
+  }
+}
