d208e605ecf0d07e316da3fd5c2a0b0270fe8386
곽승엽 <ekd7822@naver.com>
2023-05-01 16:44:27 +0900

    FEAT: BE_경쟁기능

--- another_back/src/main/java/com/example/another_back/controller/VersusController.java
+++ another_back/src/main/java/com/example/another_back/controller/VersusController.java
@@ -1,8 +1,9 @@
 package com.example.another_back.controller;
 
-import com.example.another_back.dto.Response;
+import com.example.another_back.dto.response.Response;
 import com.example.another_back.service.VersusService;
 import lombok.RequiredArgsConstructor;
+import org.json.simple.JSONArray;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,6 +26,7 @@ public class VersusController {
      */
     @GetMapping("/{runningId}")
     public ResponseEntity getVersusData(@PathVariable String runningId){
-        return Response.success(HttpStatus.OK,versusService.getVersusData(runningId));
+        JSONArray response = versusService.getVersusData(runningId);
+        return Response.success(HttpStatus.OK,response);
     }
 }
--- another_back/src/main/java/com/example/another_back/hdfs/FileIO.java
+++ another_back/src/main/java/com/example/another_back/hdfs/FileIO.java
@@ -1,37 +1,30 @@
 package com.example.another_back.hdfs;
 
 import org.apache.hadoop.conf.Configuration;
-import org.springframework.beans.factory.annotation.Value;
 
 public class FileIO {
-
-    @Value("${data.all-data}")
-    private String allData;
-
-    @Value("${data.versus-data}")
-    private String versusData;
-
     /**
      * HDFS 설정
      *
      * @return Configuration
      */
-    public Configuration getConf() {
+    public Configuration getConf(String hdfsUrl, String hdfsPort) {
         // HDFS 접속 설정
+        System.out.println(hdfsUrl + ":" + hdfsPort);
         Configuration conf = new Configuration();
-        conf.set("fs.defaultFS", "hdfs://k8b308m.p.ssafy.io:20000");
-        conf.set("dfs.client.use.datanode.hostname","true");
+        conf.set("fs.defaultFS", hdfsUrl + ":" + hdfsPort);
+        conf.set("dfs.client.use.datanode.hostname", "true");
         return conf;
     }
 
     /**
-     *  전체 데이터 반환
+     * 전체 데이터 반환
      *
      * @param runningId
      * @return
      */
-    public String allData(String runningId){
-        String url = allData+runningId+"/*";
+    public String allData(String runningId, String hdfsUrl, String hdfsPort) {
+        String url = hdfsUrl + ":" + hdfsPort + "/another/origin/" + runningId + "/*";
         return url;
     }
 
@@ -41,8 +34,8 @@ public class FileIO {
      * @param runningId
      * @return String
      */
-    public String versusData(String runningId){
-        String url = versusData+runningId+"/*";
+    public String versusData(String runningId, String hdfsUrl, String hdfsPort) {
+        String url = hdfsUrl + ":" + hdfsPort + "/another/test1/" + runningId + "/*";
         return url;
     }
 }
