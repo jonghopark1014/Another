9e6f9973c3a190b638d35225710690f5d53ea147
왕현석 <sjj02055@gmail.com>
2023-05-01 15:51:18 +0900

    FEAT: JWT이용한 인증/인가처리 구현

--- another_back/src/main/java/com/example/another_back/config/SecurityConfig.java
+++ another_back/src/main/java/com/example/another_back/config/SecurityConfig.java
@@ -1,12 +1,17 @@
 package com.example.another_back.config;
 
+import com.example.another_back.filter.JwtAuthenticationFilter;
+import com.example.another_back.filter.JwtAuthorizationFilter;
+import com.example.another_back.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -15,15 +20,25 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final CorsConfig corsConfig;
+    private final UserRepository userRepository;
+    private final JwtProvider jwtProvider;
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        JwtAuthenticationFilter authenticationFilter = new JwtAuthenticationFilter(authenticationManager(),userRepository,jwtProvider);
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .addFilter(corsConfig.corsFilter())
                 .formLogin().disable()
                 .httpBasic().disable()
+                .addFilter(authenticationFilter)
+                .addFilter(new JwtAuthorizationFilter(authenticationManager(),userRepository,jwtProvider))
                 .authorizeRequests()
                 .anyRequest().permitAll();
     }
