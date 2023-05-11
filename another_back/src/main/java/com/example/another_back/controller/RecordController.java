829b1035b0fb46c6672bb89216429d02cfd5e3f3
곽승엽 <ekd7822@naver.com>
2023-05-11 11:20:45 +0900

    FEAT: BE_달력 날짜 선택 API 기능

--- another_back/src/main/java/com/example/another_back/controller/RecordController.java
+++ another_back/src/main/java/com/example/another_back/controller/RecordController.java
@@ -1,5 +1,6 @@
 package com.example.another_back.controller;
 
+import com.example.another_back.dto.CurRunningDto;
 import com.example.another_back.dto.RunningEachHistoryDto;
 import com.example.another_back.dto.RunningHistoryResponseDto;
 import com.example.another_back.dto.SumRunningDto;
@@ -13,6 +14,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Date;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/record")
@@ -22,48 +25,81 @@ public class RecordController {
     private final RunningService runningService;
 
     @GetMapping("/{userId}")
-    public ResponseEntity getUserLevelExp(@PathVariable("userId")Long userId) {
+    public ResponseEntity getUserLevelExp(@PathVariable("userId") Long userId) {
         return Response.success(HttpStatus.OK, userService.getUserLevelExp(userId));
     }
 
     @GetMapping("/{userId}/today")
-    public ResponseEntity getTodayRecord(@PathVariable("userId")Long userId){
+    public ResponseEntity getTodayRecord(@PathVariable("userId") Long userId) {
         RunningHistoryResponseDto record = runningService.getRecord(1, userId);
         return Response.success(HttpStatus.OK, record);
     }
+
     @GetMapping("/{userId}/history/today")
-    public ResponseEntity getTodayHisotry(@PathVariable("userId")Long userId, Pageable pageable){
+    public ResponseEntity getTodayHisotry(@PathVariable("userId") Long userId, Pageable pageable) {
         Page<RunningEachHistoryDto> record = runningService.getHistory(1, userId, pageable);
         return Response.success(HttpStatus.OK, record);
     }
+
     @GetMapping("/{userId}/week")
-    public ResponseEntity getWeekRecord(@PathVariable("userId")Long userId){
+    public ResponseEntity getWeekRecord(@PathVariable("userId") Long userId) {
         RunningHistoryResponseDto record = runningService.getRecord(2, userId);
         return Response.success(HttpStatus.OK, record);
     }
+
     @GetMapping("/{userId}/history/week")
-    public ResponseEntity getWeekHisotry(@PathVariable("userId")Long userId, Pageable pageable){
+    public ResponseEntity getWeekHisotry(@PathVariable("userId") Long userId, Pageable pageable) {
         Page<RunningEachHistoryDto> record = runningService.getHistory(2, userId, pageable);
         return Response.success(HttpStatus.OK, record);
     }
+
     @GetMapping("/{userId}/month")
-    public ResponseEntity getMonthRecord(@PathVariable("userId")Long userId){
+    public ResponseEntity getMonthRecord(@PathVariable("userId") Long userId) {
         RunningHistoryResponseDto record = runningService.getRecord(3, userId);
         return Response.success(HttpStatus.OK, record);
     }
+
     @GetMapping("/{userId}/history/month")
-    public ResponseEntity getMonthHisotry(@PathVariable("userId")Long userId, Pageable pageable){
+    public ResponseEntity getMonthHisotry(@PathVariable("userId") Long userId, Pageable pageable) {
         Page<RunningEachHistoryDto> record = runningService.getHistory(3, userId, pageable);
         return Response.success(HttpStatus.OK, record);
     }
+
     @GetMapping("/{userId}/all")
-    public ResponseEntity getAllRecord(@PathVariable("userId")Long userId){
+    public ResponseEntity getAllRecord(@PathVariable("userId") Long userId) {
         SumRunningDto record = runningService.getAllRecord(userId);
         return Response.success(HttpStatus.OK, record);
     }
+
     @GetMapping("/{userId}/history/all")
-    public ResponseEntity getAllHisotry(@PathVariable("userId")Long userId, Pageable pageable){
+    public ResponseEntity getAllHisotry(@PathVariable("userId") Long userId, Pageable pageable) {
         Page<RunningEachHistoryDto> record = runningService.getHistory(4, userId, pageable);
+        return Response.success(HttpStatus.OK, record);
+    }
+
+    /**
+     * 달력에서 선택한 날짜 API
+     *
+     * @param userId
+     * @param createDate
+     * @return CurRunningDto
+     */
+    @PostMapping("/{userId}/interval")
+    public ResponseEntity getInterval(@PathVariable("userId") Long userId, Date createDate) {
+        CurRunningDto record = runningService.getIntervalRecord(userId, createDate);
+        return Response.success(HttpStatus.OK, record);
+    }
+
+    /**
+     * 달력에서 선택한 날짜 API
+     *
+     * @param userId
+     * @param createDate
+     * @return Page<RunningEachHistoryDto>
+     */
+    @PostMapping("/{userId}/history/interval")
+    public ResponseEntity getIntervalHisotry(@PathVariable("userId") Long userId, Date createDate, Pageable pageable) {
+        Page<RunningEachHistoryDto> record = runningService.getIntervalHistory(userId, createDate, pageable);
         return Response.success(HttpStatus.OK, record);
     }
 }
--- another_back/src/main/java/com/example/another_back/repository/RunningRepository.java
+++ another_back/src/main/java/com/example/another_back/repository/RunningRepository.java
@@ -27,6 +27,12 @@ public interface RunningRepository extends JpaRepository<Running, String> {
 
     List<Running> findByUserId(Long userId);
 
+    @Query(value = "select new com.example.another_back.dto.RunningEachHistoryDto(r.id, r.runningTime, r.runningDistance,r.userPace, r.createDate, r.userCalories) from Running r where r.user = :user and r.createDate = :createDate")
+    Page<RunningEachHistoryDto> findByCreateDateAndUserId(User user,Date createDate, Pageable pageable);
+
+    @Query(value = "select new com.example.another_back.dto.RunningRecordDto(avg(r.runningTime), avg(r.runningDistance), avg(r.userPace), avg(r.userCalories),sum(r.runningTime), sum(r.runningDistance), sum(r.userPace), sum(r.userCalories)) from Running r where r.user = :user and r.createDate = :createDate")
+    Optional<RunningRecordDto> findRunningRecordDtoByUserId(User user, Date createDate);
+
     @Query(value = "select new com.example.another_back.dto.RunningEachHistoryDto(r.id, r.runningTime, r.runningDistance,r.userPace, r.createDate, r.userCalories) from Running r where r.user = :user and r.createDate between :date1 and :date2")
     Page<RunningEachHistoryDto> findWithDateByUserId(User user, Date date1, Date date2, Pageable pageable);
 
@@ -36,8 +42,6 @@ public interface RunningRepository extends JpaRepository<Running, String> {
     @Query(value = "select new com.example.another_back.dto.SumRunningDto(sum(r.runningTime), sum(r.userCalories), sum(r.runningDistance), avg(r.userPace), min(r.createDate), max(r.createDate)) from Running r where r.user = :user")
     Optional<SumRunningDto> findAllRunningHistoryDtoByUserId(User user);
 
-    List<Running> findByRunningDistanceBetweenAndRunningTimeBetweenAndUserIdNot(Float startDistance, Float endDistance, String startTime, String endTime, Long userId);
-
     @Query("select coalesce(sum(r.runningDistance), 0) from Running r where r.user.id = :userId AND r.createDate between :startDate and :endDate")
     Optional<Float> SumRunningDistanceByUserIdAndCreateDateBetween(Long userId, Date startDate, Date endDate);
 
@@ -46,4 +50,6 @@ public interface RunningRepository extends JpaRepository<Running, String> {
 
     @Query("select coalesce(sum(r.runningTime), 0) from Running r where r.user.id = :userId")
     Optional<Long> SumRunningTimeByUserId(Long userId);
+
+    List<Running> findByRunningDistanceBetweenAndRunningTimeBetweenAndUserIdNot(Float startDistance, Float endDistance, String startTime, String endTime, Long userId);
 }
