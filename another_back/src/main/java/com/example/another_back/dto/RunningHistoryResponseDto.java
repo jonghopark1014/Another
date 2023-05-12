dce22c622e885c8bc507adf80bcfc320aa947329
곽승엽 <ekd7822@naver.com>
2023-05-12 17:34:56 +0900

    FEAT: BE_사용자 뱃지 가져오기

--- another_back/src/main/java/com/example/another_back/dto/RunningHistoryResponseDto.java
+++ another_back/src/main/java/com/example/another_back/dto/RunningHistoryResponseDto.java
@@ -10,9 +10,12 @@ public class RunningHistoryResponseDto {
 
     @Builder
     public RunningHistoryResponseDto(AvgRunningDto prevAvg, CurRunningDto curAvg) {
-        prevAvg.setAvgDistance(Math.round(prevAvg.getAvgDistance()*1000)/1000.0);
-        curAvg.setAvgDistance(Math.round(curAvg.getAvgDistance()*1000)/1000.0);
-        curAvg.setSumDistance(Math.round(curAvg.getSumDistance()*1000)/1000.0);
+        prevAvg.setAvgDistance(Math.round(prevAvg.getAvgDistance() * 1000) / 1000.0);
+        curAvg.setAvgDistance(Math.round(curAvg.getAvgDistance() * 1000) / 1000.0);
+        curAvg.setSumDistance(Math.round(curAvg.getSumDistance() * 1000) / 1000.0);
+        prevAvg.setAvgKcal(Math.round(prevAvg.getAvgKcal() * 1000) / 1000.0);
+        curAvg.setAvgKcal(Math.round(prevAvg.getAvgKcal() * 1000) / 1000.0);
+        curAvg.setAvgKcal(Math.round(prevAvg.getAvgKcal() * 1000) / 1000.0);
         this.prevAvg = prevAvg;
         this.curAvg = curAvg;
     }
