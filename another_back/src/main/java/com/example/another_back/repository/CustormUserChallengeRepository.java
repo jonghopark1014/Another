7ab00738b8e76d519a250af70baa3516ae54280a
곽승엽 <ekd7822@naver.com>
2023-05-14 20:34:24 +0900

    FIX: BE_API 기능 수정

--- another_back/src/main/java/com/example/another_back/repository/CustormUserChallengeRepository.java
+++ another_back/src/main/java/com/example/another_back/repository/CustormUserChallengeRepository.java
@@ -2,8 +2,6 @@ package com.example.another_back.repository;
 
 import com.example.another_back.entity.Challenge;
 
-import java.util.Optional;
-
 public interface CustormUserChallengeRepository {
-    Challenge findTop1ByUserAndStatusNotaAndWithChallenge(Long userId, String status);
+    Challenge findTop1ByUserIdAndStatusNotAndWithChallenge(Long userId, String status);
 }
