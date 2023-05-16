9ecc527022451a707f6fb3053066d4d9a914e250
baamkyu <baamkyu@gmail.com>
2023-05-16 15:56:29 +0900

    FEAT: 로그인 UI 수정
    Former-commit-id: 3bb575c67766b888443e7dfc40717530b0949ef8

--- another_front/lib/screens/account/login.dart
+++ another_front/lib/screens/account/login.dart
@@ -46,114 +46,115 @@ class _LoginPageState extends State<LoginPage> {
       });
     });
 
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: BACKGROUND_COLOR,
-        body: SingleChildScrollView(
-          keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-          child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                IntroHeader(),
-                SizedBox(height: 40.0),
-                TextField(
-                  onChanged: (String value) {
-                    username = value;
-                  },
-                  controller: emailController,
-                  focusNode: idFocusNode,
-                  decoration: InputDecoration(
-                    labelText: '아이디(이메일)',
-                    labelStyle: TextStyle(color: SERVEONE_COLOR),
-                    prefixIcon: Opacity(
-                      opacity: idFocusNode.hasFocus ? 1 : 0.5,
-                      child: Icon(Icons.email, color: SERVEONE_COLOR),
-                    ),
-                    border: UnderlineInputBorder(
-                      borderSide: BorderSide(color: SERVEONE_COLOR),
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: SERVEONE_COLOR),
-                    ),
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: SERVEONE_COLOR),
-                    ),
-                  ),
-                  style: TextStyle(color: SERVEONE_COLOR),
-                ),
-                SizedBox(height: 16),
-                TextField(
-                  controller: pwController,
-                  onChanged: (String value) {
-                    password = value;
-                  },
-                  focusNode: pwFocusNode,
-                  decoration: InputDecoration(
-                    labelText: '비밀번호',
-                    labelStyle: TextStyle(color: SERVEONE_COLOR),
-                    prefixIcon: Opacity(
-                      opacity: pwFocusNode.hasFocus ? 1 : 0.5,
-                      child: Icon(Icons.lock, color: SERVEONE_COLOR),
-                    ),
-                    border: UnderlineInputBorder(
-                      borderSide: BorderSide(color: SERVEONE_COLOR),
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: SERVEONE_COLOR),
-                    ),
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: SERVEONE_COLOR),
+    return Scaffold(
+      backgroundColor: BACKGROUND_COLOR,
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IntroHeader(),
+                  SizedBox(height: 40.0),
+                  TextField(
+                    onChanged: (String value) {
+                      username = value;
+                    },
+                    controller: emailController,
+                    focusNode: idFocusNode,
+                    decoration: InputDecoration(
+                      labelText: '아이디(이메일)',
+                      labelStyle: TextStyle(color: SERVEONE_COLOR),
+                      prefixIcon: Opacity(
+                        opacity: idFocusNode.hasFocus ? 1 : 0.5,
+                        child: Icon(Icons.email, color: SERVEONE_COLOR),
+                      ),
+                      border: UnderlineInputBorder(
+                        borderSide: BorderSide(color: SERVEONE_COLOR),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: SERVEONE_COLOR),
+                      ),
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: SERVEONE_COLOR),
+                      ),
                     ),
                     style: TextStyle(color: SERVEONE_COLOR),
                   ),
-                  style: TextStyle(color: SERVEONE_COLOR),
-                  obscureText: true,
-                ),
-                SizedBox(height: 16),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    TextButton(
-                      onPressed: () {
-                        // 회원가입 페이지로 이동하는 로직 작성
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => SignupPage()));
-                      },
-                      child: Text(
-                        '회원가입',
-                        style: TextStyle(
-                          color: SERVEONE_COLOR,
+                  SizedBox(height: 16),
+                  TextField(
+                    controller: pwController,
+                    onChanged: (String value) {
+                      password = value;
+                    },
+                    focusNode: pwFocusNode,
+                    decoration: InputDecoration(
+                      labelText: '비밀번호',
+                      labelStyle: TextStyle(color: SERVEONE_COLOR),
+                      prefixIcon: Opacity(
+                        opacity: pwFocusNode.hasFocus ? 1 : 0.5,
+                        child: Icon(Icons.lock, color: SERVEONE_COLOR),
+                      ),
+                      border: UnderlineInputBorder(
+                        borderSide: BorderSide(color: SERVEONE_COLOR),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: SERVEONE_COLOR),
+                      ),
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: SERVEONE_COLOR),
+                      ),
+                      // style: TextStyle(color: SERVEONE_COLOR),
+                    ),
+                    style: TextStyle(color: SERVEONE_COLOR),
+                    obscureText: true,
+                  ),
+                  SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextButton(
+                        onPressed: () {
+                          // 회원가입 페이지로 이동하는 로직 작성
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => SignupPage()));
+                        },
+                        child: Text(
+                          '회원가입',
+                          style: TextStyle(
+                            color: SERVEONE_COLOR,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 16),
-                FractionallySizedBox(
-                  widthFactor: 1.0,
-                  child: ElevatedButton(
-                    onPressed: () async {
-                      final resp = await dio.post(
-                        '$baseUrl/user/login',
-                        data: {
-                          'username': username,
-                          'password': password,
-                        },
-                      );
-                      final refreshToken = resp.headers['refresh']?[0];
-                      final accessToken = resp.headers['Authorization']?[0];
-                      final userId = resp.data['userId'];
-                      final nickname = resp.data['nickname'];
-                      final weight = resp.data['weight'];
-                      final height = resp.data['height'];
+                    ],
+                  ),
+                  SizedBox(height: 16),
+                  FractionallySizedBox(
+                    widthFactor: 1.0,
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        final resp = await dio.post(
+                          '$baseUrl/user/login',
+                          data: {
+                            'username': username,
+                            'password': password,
+                          },
+                        );
+                        final refreshToken = resp.headers['refresh']?[0];
+                        final accessToken = resp.headers['Authorization']?[0];
+                        final userId = resp.data['userId'];
+                        final nickname = resp.data['nickname'];
+                        final weight = resp.data['weight'];
+                        final height = resp.data['height'];
 
                         if (userId != null) {
                           Provider.of<UserInfo>(context, listen: false)
-                          .updateUserInfo(userId, nickname, weight, height);
+                              .updateUserInfo(userId, nickname, weight, height);
                         }
                         await storage.write(
                             key: REFRESH_TOKEN_KEY, value: refreshToken);
@@ -161,19 +162,21 @@ class _LoginPageState extends State<LoginPage> {
                             key: ACCESS_TOKEN_KEY, value: accessToken);
 
                         Navigator.of(context).pushAndRemoveUntil(
-                          MaterialPageRoute(
-                            builder: (_) => HomeScreen(),
-                          ),
-                          (route) => false);
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: MAIN_COLOR,
+                            MaterialPageRoute(
+                              builder: (_) => HomeScreen(),
+                            ),
+                            (route) => false);
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: MAIN_COLOR,
+                        minimumSize: Size(double.infinity, 40)
+                      ),
+                      child: Text('로그인'),
                     ),
-                    child: Text('로그인'),
                   ),
-                ),
-                SizedBox(height: 16),
-              ],
+                  SizedBox(height: 16),
+                ],
+              ),
             ),
           ),
         ),
