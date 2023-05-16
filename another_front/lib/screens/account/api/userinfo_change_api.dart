c853252c71cdc471cc723b1efa9c4b22279a247d
baamkyu <baamkyu@gmail.com>
2023-05-16 11:09:17 +0900

    FEAT: 프로필 변경
    Former-commit-id: b060d43fec59add3a525c2087d664d3a4b152b17

--- another_front/lib/screens/account/api/userinfo_change_api.dart
+++ another_front/lib/screens/account/api/userinfo_change_api.dart
@@ -1,6 +1,9 @@
 import 'dart:io';
 import 'dart:convert';
+import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
+import 'package:another/main.dart';
 
 const String _baseUrl = 'https://k8b308.p.ssafy.io/api';
 
@@ -25,7 +28,7 @@ class UserInfoChangeApi {
       print(await response.body);
       print('키몸무게 변경 성공');
     } else {
-      print('요청실패: ${response.statusCode}');
+      print('키몸무게닉네임 요청실패: ${response.statusCode}');
     }
   }
 
@@ -36,6 +39,7 @@ class UserInfoChangeApi {
 
     // HTTP PATCH 요청 보내기
     var request = http.MultipartRequest('PATCH', url);
+
     request.files.add(await http.MultipartFile.fromPath(
       'profileImage',
       pickedFile!.path,
@@ -48,7 +52,7 @@ class UserInfoChangeApi {
       print(await response.stream.bytesToString());
       print('성공');
     } else {
-      print('요청실패: ${response.statusCode}');
+      print('프로필사진 변경 요청실패: ${response.statusCode}');
     }
   }
 }
--- another_front/lib/screens/account/edit.dart
+++ another_front/lib/screens/account/edit.dart
@@ -10,6 +10,8 @@ import './widgets/complete_button.dart';
 import './widgets/image_picker.dart';
 import './api/userinfo_change_api.dart';
 import 'package:another/screens/account/api/nickname_check_api.dart';
+import 'package:another/screens/record/api/user_level_exp_api.dart';
+import 'package:another/screens/record/record.dart';
 
 class ProfileEditPage extends StatefulWidget {
   const ProfileEditPage({Key? key}) : super(key: key);
@@ -61,11 +63,14 @@ class _ProfileEditPageState extends State<ProfileEditPage> {
     return null; // 유효성 검사 통과
   }
 
-  void _onSavePressed() async {
+  void _onSavePressed(BuildContext context) async {
     // 프로필 사진 수정한 경우
     if (isNicknamePossible == true) {
       if (profileImg != null) {
         await UserInfoChangeApi.profileImgChange(pickedFile: profileImg!);
+        // context.read<UserInfo>().updateProfileImg(profileImg);
+        print('profileImg: $profileImg');
+        print('Provider에 프로필사진 변경');
       }
       // 유저 정보
       await UserInfoChangeApi.userInfoChange(
@@ -73,7 +78,14 @@ class _ProfileEditPageState extends State<ProfileEditPage> {
         weight: _weight,
         nickname: _nicknameController.text,
       );
-      Navigator.pop(context); // 뒤로가기
+      context.read<UserInfo>().updateNicknameHeightWeight(_nicknameController.text, _height, _weight);
+      // print('Provider에 닉네임키몸무게 변경');
+      // 유저정보 가져오는 API 통신 다시
+      await UserLevelExpApi.getUserLevelExp();
+      Navigator.of(context).pushAndRemoveUntil(
+          MaterialPageRoute(
+            builder: (_) => RecordTab(),
+          ),(route) => false);
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('사용자 정보가 변경되었습니다.')),
       );
@@ -104,13 +116,15 @@ class _ProfileEditPageState extends State<ProfileEditPage> {
 
   @override
   Widget build(BuildContext context) {
+    print(profileImg);
     return Scaffold(
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           IntroHeader(),
           SizedBox(height: 16),
           ProfileImage(
-            profileImg: _profileImgUrl,
+            profileImg: context.watch<UserInfo>().profileImg,
             key: _imagePickerKey,
             onFileChanged: (img) => setState(() => profileImg = img),
           ),
@@ -197,7 +211,7 @@ class _ProfileEditPageState extends State<ProfileEditPage> {
               CompleteButton(
                 text: '수정 완료',
                 onPressed: () {
-                  _onSavePressed();
+                  _onSavePressed(context);
                 },
               ),
             ],
